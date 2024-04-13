1c093356b2ac04f72ea73b9a218227317db1de0d
kaminsca <clark.a.kaminsky@vanderbilt.edu>
2024-04-13 14:56:39 -0400

    started gemini api query, loading secret from env

--- server/app.py
+++ server/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS, cross_origin
+import google.generativeai as genai
+from dotenv import load_dotenv
+import PIL.Image
+import os
+
 
 app = Flask(__name__)
 CORS(app) # This will enable CORS for all routes
@@ -13,7 +18,24 @@ def hello_world():
 
 @app.route("/")
 def home():
+    gemini_query()
     return render_template('index.html')
 
+@app.route("/gemini", methods = ['GET'])
+def gemini_query():
+    genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
+    model = genai.GenerativeModel('gemini-pro')
+    #TODO: replace image sources!
+    # img1 = PIL.Image.open('image.jpg')
+    prompt = 'Say hi to me!'
+    prompt_list = []
+    # prompt_list.append(img1)
+    prompt_list.append(prompt)
+    response = model.generate_content(prompt_list)
+    console.log(response)
+    #TODO: add response to database
+    return response
+
 if __name__ == '__main__':
-   app.run()
+    load_dotenv()
+    app.run()
